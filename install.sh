4051fcad9b25ce1a840503b1770bd3d7fa84f71e
Denis <orliange@free.fr>
2019-05-12 09:32:43 +0200

    Update install.sh
    Mise à jour de Docker-Compose en 1.24.0

--- install.sh
+++ install.sh
@@ -40,7 +40,7 @@ apt-get install docker-ce
 echo ""
 echo -e "${bleuclair}Installation de Docker-Compose${neutre}"
 echo ""
-curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 #
 #Portainer
